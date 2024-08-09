649affea7b82bc12a9e81b757111c97ba726f37e
Tomás Cornelles <menos5@gmail.com>
2024-08-09 13:31:27 +0200

    Create teams.js

---
+++ app/data/teams.js
@@ -0,0 +1,353 @@
+export const teams = [
+  {
+    name: 'FC Barcelona',
+    kits: [
+      {
+        name: 'home',
+        colors: {
+          jersey: 'navy',
+          pants: 'navy',
+          socks: 'navy',
+          layers: [
+            ['sideLeft','crimson'],
+            ['sleeveRight','crimson'],
+          ],
+        }
+      },
+      {
+        name: 'away',
+        colors: {
+          jersey: '#333',
+          pants: '#333',
+          socks: '#333',
+          layers: [
+            ['checked','#444'],
+          ],
+        }
+      },
+      {
+        name: 'third',
+        colors: {
+          jersey: 'palegreen',
+          pants: 'palegreen',
+          socks: 'palegreen',
+          layers: [
+            ['checked','lightgreen'],
+          ],
+        }
+      },
+      {
+        name: 'special',
+        colors: {
+          jersey: 'darkorange',
+          pants: 'darkorange',
+          socks: 'darkorange',
+          layers: [
+            ['sideLeft','crimson'],
+            ['stripeVerticalLeft','navy'],
+          ],
+        }
+      },
+      {
+        name: 'el clasico',
+        colors: {
+          jersey: 'navy',
+          pants: 'navy',
+          socks: 'navy',
+          layers: [
+            ['stripesVertical','crimson'],
+          ],
+        }
+      },
+    ]
+  },
+
+  {
+    name: 'Parma',
+    kits: [
+      {
+        name: 'home',
+        colors: {
+          jersey: 'whitesmoke',
+          pants: 'gainsboro',
+          socks: 'whitesmoke',
+          layers: [
+            ['cross','black'],
+          ],
+        }
+      },
+    ]
+  },
+
+  {
+    name: 'Boca Juniors',
+    kits: [
+      {
+        name: 'home',
+        colors: {
+          jersey: 'darkblue',
+          pants: 'darkblue',
+          socks: 'gold',
+          layers: [
+            ['stripeHorizontal','gold'],
+          ],
+        }
+      },
+    ]
+  },
+
+  {
+    name: 'River Plate',
+    kits: [
+      {
+        name: 'home',
+        colors: {
+          jersey: 'whitesmoke',
+          pants: 'black',
+          socks: 'whitesmoke',
+          layers: [
+            ['stripeDiagonal','red'],
+          ],
+        }
+      },
+    ]
+  },
+
+  {
+    name: 'Flamingo',
+    kits: [
+      {
+        name: 'home',
+        colors: {
+          jersey: 'black',
+          pants: 'black',
+          socks: 'whitesmoke',
+          layers: [
+            ['stripesHorizontal','red'],
+          ],
+        }
+      },
+    ]
+  },
+
+  {
+    name: 'Croacia',
+    kits: [
+      {
+        name: 'home',
+        colors: {
+          jersey: 'whitesmoke',
+          pants: 'whitesmoke',
+          socks: 'blue',
+          layers: [
+            ['checked','red'],
+          ],
+        }
+      },
+      {
+        name: 'away',
+        colors: {
+          jersey: 'whitesmoke',
+          pants: 'blue',
+          socks: 'blue',
+          layers: [
+            ['checked','red'],
+            ['sideRight','blue'],
+            ['sleeveLeft','blue'],
+            ['sleeveRight','blue'],
+          ],
+        }
+      },
+    ]
+  },
+
+  {
+    name: 'AS Monaco',
+    kits: [
+      {
+        name: 'home',
+        colors: {
+          jersey: 'whitesmoke',
+          pants: 'whitesmoke',
+          socks: 'red',
+          layers: [
+            ['sideDiagonal','red'],
+            ['sleeveLeft','red'],
+          ],
+        }
+      },
+      {
+        name: 'away',
+        colors: {
+          jersey: 'blue',
+          pants: 'blue',
+          socks: 'blue',
+          layers: [
+            ['sideDiagonal','gold'],
+            ['sleeveLeft','gold'],
+          ],
+        }
+      },
+    ]
+  },
+
+  {
+    name: 'Spain 96',
+    kits: [
+      {
+        name: 'home',
+        colors: {
+          jersey: 'crimson',
+          pants: 'navy',
+          socks: 'black',
+          layers: [
+            ['stripeVerticalLeft','navy'],
+            ['sleeveLeft','navy'],
+          ],
+        }
+      },
+      {
+        name: 'away',
+        colors: {
+          jersey: 'navy',
+          pants: 'navy',
+          socks: 'navy',
+          layers: [
+            ['stripeVerticalLeft','gold'],
+            ['sleeveLeft','navy'],
+          ],
+        }
+      },
+      {
+        name: 'third',
+        colors: {
+          jersey: 'whitesmoke',
+          pants: 'whitesmoke',
+          socks: 'whitesmoke',
+          layers: [
+            ['sleeveLeft','crimson'],
+            ['sleeveRight','crimson'],
+          ],
+        }
+      },
+    ]
+  },
+  {
+    name: 'Spain 98',
+    kits: [
+      {
+        name: 'home 98',
+        colors: {
+          jersey: 'navy',
+          pants: 'navy',
+          socks: 'black',
+          layers: [
+            ['checked','gold'],
+            ['sideLeft', 'crimson'],
+            ['stripeVertical', 'crimson'],
+            ['sleeveLeft','crimson'],
+            ['sleeveRight','crimson'],
+          ],
+        }
+      },
+      {
+        name: 'away 98',
+        colors: {
+          jersey: 'red',
+          pants: 'navy',
+          socks: 'navy',
+          layers: [
+            ['checked','gold'],
+            ['sideLeft', 'navy'],
+            ['stripeVertical', 'navy'],
+            ['sleeveLeft','navy'],
+            ['sleeveRight','navy'],
+          ],
+        }
+      },
+      {
+        name: 'third 98',
+        colors: {
+          jersey: 'gold',
+          pants: 'whitesmoke',
+          socks: 'whitesmoke',
+          layers: [
+            ['checked','red'],
+            ['sideLeft', 'whitesmoke'],
+            ['stripeVertical', 'whitesmoke'],
+            ['sleeveLeft','whitesmoke'],
+            ['sleeveRight','whitesmoke'],
+          ],
+        }
+      },
+    ]
+  },
+
+  {
+    name: 'PSG',
+    kits: [
+      {
+        name: 'home',
+        colors: {
+          jersey: 'navy',
+          pants: 'navy',
+          socks: 'navy',
+          layers: [
+            ['stripeVertical','red'],
+            ['sideRight','navy'],
+          ],
+        }
+      },
+    ]
+  },
+
+  {
+    name: 'AJAX',
+    kits: [
+      {
+        name: 'home',
+        colors: {
+          jersey: 'whitesmoke',
+          pants: 'whitesmoke',
+          socks: 'whitesmoke',
+          layers: [
+            ['stripeVertical','red'],
+          ],
+        }
+      },
+    ]
+  },
+
+  {
+    name: 'Arsenal',
+    kits: [
+      {
+        name: 'home',
+        colors: {
+          jersey: 'red',
+          pants: 'whitesmoke',
+          socks: 'whitesmoke',
+          layers: [
+            ['sleeveLeft','whitesmoke'],
+            ['sleeveRight','whitesmoke'],
+          ],
+        }
+      },
+    ]
+  },
+
+  {
+    name: 'Manchester United',
+    kits: [
+      {
+        name: 'home',
+        colors: {
+          jersey: 'red',
+          pants: 'whitesmoke',
+          socks: 'whitesmoke',
+          layers: [],
+        }
+      },
+    ]
+  },
+]
